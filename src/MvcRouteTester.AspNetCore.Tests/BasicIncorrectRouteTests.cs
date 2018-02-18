f0c41096a0f6061fbccf69881451d8b27b33c59b
nwendel <niklas.wendel@gmail.com>
2018-02-18 20:22:22 +0100

    Added basic incorrect route tests

---
+++ src/MvcRouteTester.AspNetCore.Tests/BasicIncorrectRouteTests.cs
@@ -0,0 +1,71 @@
+﻿#region License
+// Copyright (c) Niklas Wendel 2018
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+// 
+// http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+#endregion
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+using Xunit.Sdk;
+using TestWebApplication.Controllers;
+
+namespace MvcRouteTester.AspNetCore.Tests
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BasicIncorrectRouteTests : IClassFixture<TestServerFixture>
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly TestServer _server;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public BasicIncorrectRouteTests(TestServerFixture testServerFixture)
+        {
+            _server = testServerFixture.Server;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void ThrowsOnMapsToIncorrectController()
+        {
+            Assert.Throws<EqualException>(() =>
+                RouteAssert.For(
+                    _server,
+                    request => request.WithPathAndQuery("/simple-attribute-route"),
+                    route => route.MapsTo<InvalidController>(a => a.Default())));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void ThrowsOnMapsToIncorrectActionMethodName()
+        {
+            Assert.Throws<EqualException>(() =>
+                RouteAssert.For(
+                    _server,
+                    request => request.WithPathAndQuery("/simple-attribute-route"),
+                    route => route.MapsTo<HomeController>(a => a.SimpleAttributeRouteAsync())));
+        }
+        
+    }
+
+}
