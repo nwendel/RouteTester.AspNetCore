29302883623919affdc1907f69d4b4e435b00f60
Niklas Wendel <niklas.wendel@gmail.com>
2018-02-21 16:56:56 +0100

    Added ForParameter unit test

--- src/MvcRouteTester.AspNetCore.Tests/PostFormDataTests.cs
+++ src/MvcRouteTester.AspNetCore.Tests/PostFormDataTests.cs
@@ -58,7 +58,13 @@ namespace MvcRouteTester.AspNetCore.Tests
                         { "FirstName", "Niklas" },
                         { "LastName", "Wendel" }
                     }),
-                routeAssert => routeAssert.MapsTo<PostController>(a => a.WithPerson(Args.Any<Person>())));
+                routeAssert => routeAssert
+                    .MapsTo<PostController>(a => a.WithPerson(Args.Any<Person>()))
+                    .ForParameter<Person>("person", p =>
+                    {
+                        Assert.Equal("Niklas", p.FirstName);
+                        Assert.Equal("Wendel", p.LastName);
+                    }));
         }
 
     }
