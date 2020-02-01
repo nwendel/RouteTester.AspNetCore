e901701703542e4c30363aaeeb9b3c73c9ddf264
chsakell <chsakell@gmail.com>
2020-02-01 23:41:25 +0200

    Add support for testing with JSON post data

--- src/MvcRouteTester.AspNetCore/Internal/Extensions.cs
+++ src/MvcRouteTester.AspNetCore/Internal/Extensions.cs
@@ -15,7 +15,12 @@
 #endregion
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace MvcRouteTester.AspNetCore.Internal
 {
@@ -78,6 +83,36 @@ namespace MvcRouteTester.AspNetCore.Internal
 
         #endregion
 
+        #region Object / Get HttpContent
+
+        public static HttpContent ToHttpContent(this object data)
+        {
+            HttpContent httpContent = null;
+
+            if (data != null)
+            {
+                var ms = new MemoryStream();
+                SerializeJsonIntoStream(data, ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                httpContent = new StreamContent(ms);
+                httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
+
+            return httpContent;
+        }
+
+        private static void SerializeJsonIntoStream(object value, Stream stream)
+        {
+            using (var sw = new StreamWriter(stream, new UTF8Encoding(false), 1024, true))
+            using (var jtw = new JsonTextWriter(sw) { Formatting = Formatting.None })
+            {
+                var js = new JsonSerializer();
+                js.Serialize(jtw, value);
+                jtw.Flush();
+            }
+        }
+
+        #endregion
     }
 
 }
