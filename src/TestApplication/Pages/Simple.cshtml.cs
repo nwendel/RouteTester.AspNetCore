d3f7b81d251cc766b97e40d52e1101d9b808a8af
niklaswendel <niklas.wendel@gmail.com>
2023-01-29 16:13:42 +0100

    Initial work on supporting razor pages

---
+++ src/TestApplication/Pages/Simple.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestApplication.Pages;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name should match first type name", Justification = "Not sure")]
+public class SimplePageModel : PageModel
+{
+    public void OnGet()
+    {
+        // Nothing to see here
+    }
+}
--- src/TestApplication/Program.cs
+++ src/TestApplication/Program.cs
@@ -1,18 +1,18 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddRazorPages();
 builder.Services.AddMvc();
 
 var app = builder.Build();
 
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.UseEndpoints(x =>
 {
+    x.MapRazorPages();
     x.MapControllers();
 });
 
